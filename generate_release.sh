1eef245105217f2fa694bd0347552ab507c0240e
Chris Guest <chris.guest75@hotmail.com>
2020-03-24 22:13:45 +0000

    Add options to the generate script (#1054)
    Problem
    Want to support both options easily
    
    Notes
    Usage: generate_release.sh release|deployment

--- generate_release.sh
+++ generate_release.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [[ -z $1 ]]; then 
+    echo "Usage: generate_release.sh release|deployment"    
+    exit 1
+fi 
+
 if [[ ! $(which gomplate) ]]; then
     echo "gomplate tool not found.  Please install and retry"
     exit
@@ -26,28 +31,32 @@ git log  --pretty=format:"'%h', '%an', '%s'" 58ee502..5144e24 > ./output/2.0.txt
 git log  --pretty=format:"'%h', '%an', '%s'" 5144e24..7130ef6 > ./output/2.1.txt
 git log  --pretty=format:"'%h', '%an', '%s'" 7130ef6..ab4ffc5 > ./output/2.2.txt
 git log  --pretty=format:"'%h', '%an', '%s'" ab4ffc5..8de801d > ./output/2.17.txt
+echo ""
 
+if [[ $1 == "release" ]]; then 
+    TEMPLATE=./release_notes.gomplate
 
-# TEMPLATE=./release_notes.gomplate
+    for filename in ./output/*.txt; do
+        version=$(basename ${filename} .txt)
+        echo "{'version':'${version}', 'url':'https://github.com/chrisguest75/git_examples'}" | gomplate --file ${TEMPLATE} -c version=stdin:///in.json -c .=${filename} > ./output/${version}.md  
+    done
 
-# for filename in ./output/*.txt; do
-#     version=$(basename ${filename} .txt)
-#     echo "{'version':'${version}', 'url':'https://github.com/chrisguest75/git_examples'}" | gomplate --file ${TEMPLATE} -c version=stdin:///in.json -c .=${filename} > ./output/${version}.md  
-# done
+    echo "# RELEASE NOTES" > RELEASE_NOTES.md
+    for filename in $(ls ./output | grep md | sort -Vr); do
+        cat "./output/${filename}" >> RELEASE_NOTES.md
+    done
+elif [[ $1 == "deployment" ]]; then 
+    TEMPLATE=./deployed.gomplate
 
-# echo "# RELEASE NOTES" > RELEASE_NOTES.md
-# for filename in $(ls ./output | grep md | sort -Vr); do
-#     cat "./output/${filename}" >> RELEASE_NOTES.md
-# done
+    for filename in ./output/*.txt; do
+        version=$(basename ${filename} .txt)
+        echo "{'version':'${version}', 'url':'https://github.com/chrisguest75/git_examples'}" | gomplate --file ${TEMPLATE} -c emojis=deployment_emojis.json -c version=stdin:///in.json -c .=${filename} > ./output/${version}.md  
+    done
 
-TEMPLATE=./deployed.gomplate
-
-for filename in ./output/*.txt; do
-    version=$(basename ${filename} .txt)
-    echo "{'version':'${version}', 'url':'https://github.com/chrisguest75/git_examples'}" | gomplate --file ${TEMPLATE} -c emojis=deployment_emojis.json -c version=stdin:///in.json -c .=${filename} > ./output/${version}.md  
-done
-
-echo "# DEPLOYMENTS" > DEPLOYMENTS.md
-for filename in $(ls ./output | grep md | sort -Vr); do
-    cat "./output/${filename}" >> DEPLOYMENTS.md
-done
+    echo "# DEPLOYMENTS" > DEPLOYMENTS.md
+    for filename in $(ls ./output | grep md | sort -Vr); do
+        cat "./output/${filename}" >> DEPLOYMENTS.md
+    done
+else
+    echo "$1 not recognised"
+fi
