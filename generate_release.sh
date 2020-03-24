44e2000c4623905208d649263452f74fc088406c
Chris Guest <chris.guest@condenastint.com>
2020-03-24 10:06:12 +0000

    Create all versions (#1010)
    Problem
    Generate versions from a list of files
    
    Solution
    Run generate_release.sh

---
+++ generate_release.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+git log  --pretty=format:"%h %an%x09%s"
+
+git log  --pretty=format:"'%h', '%an', '%s'" 162856a..ea5f6b1 > ./output/1.0.txt
+git log  --pretty=format:"'%h', '%an', '%s'" ea5f6b1..0ea7306 > ./output/1.1.txt
+git log  --pretty=format:"'%h', '%an', '%s'" 0ea7306..acf1304 > ./output/1.2.txt
+
+for filename in ./output/*.txt; do
+    version=$(basename ${filename} .txt)
+    cat ${filename} | gomplate --file ./release_notes.gomplate -c .=stdin://${version}.txt > ./output/${version}.md  
+done
+
