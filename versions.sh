2d2a792bb800ddd07ab4ca158782d74e6605eeb6
Chris Guest <chris.guest@condenastint.com>
2020-03-29 20:45:52 +0100

    Add a ranges file (#1110)
    Problem
    Seperate the ranges file.
    
    Solution
    Loop through a csv file.

--- versions.sh
+++ versions.sh
@@ -7,24 +7,40 @@ else
     return 1
 fi
 
+function trim() {
+    : ${1?"${FUNCNAME[0]}(string) - missing string argument"}
+
+    if [[ -z ${1} ]]; then 
+        echo ""
+        return
+    fi
+    # remove an 
+    trimmed=${1##*( )}
+    echo ${trimmed%%*( )}
+}
+
 function process() {
+    : ${1?"${FUNCNAME[0]}(basepath, ranges_filepath) - missing basepath argument"}
+
     local basepath="./"
     if [[ -n $1 ]]; then 
         basepath=$1
     fi
+    local ranges_filepath="./ranges.csv"
+    if [[ -n $2 ]]; then 
+        ranges_filepath=$2
+    fi
+
+    if [[ ! -f ${ranges_filepath} ]]; then
+        echo "${ranges_filepath} not found"
+        return 1
+    fi 
+
     basepath="${basepath}/"
 
-    git log  --pretty=format:"'%h', '%an', '%s'" 162856a..ea5f6b1 > ${basepath}1.0.txt
-    git log  --pretty=format:"'%h', '%an', '%s'" ea5f6b1..0ea7306 > ${basepath}1.1.txt
-    git log  --pretty=format:"'%h', '%an', '%s'" 0ea7306..acf1304 > ${basepath}1.2.txt
-    git log  --pretty=format:"'%h', '%an', '%s'" acf1304..58ee502 > ${basepath}1.3.txt
-    git log  --pretty=format:"'%h', '%an', '%s'" 58ee502..5144e24 > ${basepath}2.0.txt
-    git log  --pretty=format:"'%h', '%an', '%s'" 5144e24..7130ef6 > ${basepath}2.1.txt
-    git log  --pretty=format:"'%h', '%an', '%s'" 7130ef6..ab4ffc5 > ${basepath}2.2.txt
-    git log  --pretty=format:"'%h', '%an', '%s'" ab4ffc5..8de801d > ${basepath}2.17.txt
-    git log  --pretty=format:"'%h', '%an', '%s'" 8de801d..5943833 > ${basepath}2.20.txt
-    git log  --pretty=format:"'%h', '%an', '%s'" 5943833..acf8d2b > ${basepath}2.21.txt
-    git log  --pretty=format:"'%h', '%an', '%s'" acf8d2b..24781ca > ${basepath}2.22.txt
-    git log  --pretty=format:"'%h', '%an', '%s'" 24781ca..6e6b77c > ${basepath}3.0.txt
-    git log  --pretty=format:"'%h', '%an', '%s'" 6e6b77c..25b9955 > ${basepath}3.1.txt
+    while IFS=, read -r rev1 rev2 version
+    do
+        echo "$(trim $version) is between $(trim $rev1) and $(trim $rev2)"
+        git log  --pretty=format:"'%h', '%an', '%s'" $(trim $rev1)..$(trim $rev2) > ${basepath}$(trim $version).txt
+    done < ${ranges_filepath}
 }
