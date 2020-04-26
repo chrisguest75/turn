5635054f8372bf1bf198e1bbcdcdbf73d84eae35
Christopher Guest <chrisguest75@users.noreply.github.com>
2020-04-26 18:21:50 +0100

    add shellcheck (#18)
    * Fix the scripts for shellcheck and add tests.
    
    * Add shellcheck to docker

--- tags-to-ranges.sh
+++ tags-to-ranges.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 function trim() {
-    : ${1?"${FUNCNAME[0]}(string) - missing string argument"}
+    : "${1?\"${FUNCNAME[0]}(string) - missing string argument\"}"
 
     if [[ -z ${1} ]]; then 
         echo ""
@@ -9,6 +9,7 @@ function trim() {
     fi
     # remove an 
     trimmed=${1##*( )}
+    # shellcheck disable=SC2086
     echo ${trimmed%%*( )}
 }
 
@@ -22,18 +23,21 @@ function process() {
     # Do not log messages or debug 
     local branch=master
     local previous_tag=0.0
-    local depth=$(expr $(git --no-pager rev-list --first-parent --count ${branch}) - 1)
+    local depth=$(( $(git --no-pager rev-list --first-parent --count ${branch}) - 1))
     #local depth=$(git --no-pager rev-list --first-parent --count master)
+    # shellcheck disable=SC2155
     local previous_id=$(git --no-pager rev-list -n 1 --first-parent ${branch}~${depth}) 
     local current_tag=
     local current_id=
 
     if [[ -n $(git --no-pager tag --list -n1) ]]; then
+        # shellcheck disable=SC2034
         while IFS= read -r version message
         do
-            current_tag="$(trim $version)"
-            local current_id=$(git --no-pager rev-list -n 1 ${current_tag})
-            echo "$(trim $previous_id), $(trim $current_id), $(trim $current_tag)"
+            current_tag=$(trim "$version")
+            # shellcheck disable=SC2155
+            local current_id=$(git --no-pager rev-list -n 1 "${current_tag}")
+            echo "$(trim "$previous_id"), $(trim "$current_id"), $(trim "$current_tag")"
 
             previous_tag=$current_tag
             previous_id=$current_id
@@ -42,10 +46,10 @@ function process() {
     if [[ -n $include_next && $include_next == true ]]; then
         # use HEAD as it is what is next on current branch.
         current_id=$(git --no-pager rev-list -n 1 HEAD)
-        if [[ $previous_id != $current_id ]]; then 
-            echo "$(trim $previous_id), $(trim $current_id), Next"
+        if [[ $previous_id != "$current_id" ]]; then 
+            echo "$(trim "$previous_id"), $(trim "$current_id"), Next"
         fi 
     fi
 }
 
-process $1
+process "$1"
